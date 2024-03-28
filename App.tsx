92893a568660bd6c0dfb1c5b4e48ab652e57bb59
Joao Andrade <joaovictorvieira.23@hotmail.com>
2024-03-27 21:43:17 -0300

    chore: initial structure

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+
+import { Dashboard } from './src/screens/Dashboard';
+import theme from './src/global/styles/theme';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,18 @@
+export default {
+  colors: {
+    primary: '#880808',
+    secondary: '#FFDB58',
+    secondary_light: 'rgb(255, 219, 88, 0.3)',
+    
+    success: '#12A454',
+    success_light: 'rgba(18, 164, 84, 0.5)',
+
+    attention: '#E83F5B',
+    attention_light: 'rgba(232, 63, 91, 0.5)',
+
+    shape: '#ffffff',
+    title: '#363f5f',
+    text: '#969CB2',
+    background: '#F0F2F5'
+  }
+}
